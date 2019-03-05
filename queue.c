937f49c2c5ea05b72a158ee6d281a1b875d6633e
Yin Zhou <31861121+yinz87@users.noreply.github.com>
2019-03-04 22:06:53 -0500

    Update queue.c
    update

--- queue.c
+++ queue.c
@@ -19,17 +19,19 @@ void *push(node_t **head, process proc){
         node
     }
     
-    //or
-    
-    node_t * new_node = node_t -> malloc(sizeof(node_t));
-	new_node -> process = process;
-	new_node -> next = *head;
-	*head = new_node;
-    
-}
+    //or add item to end of list
+	node_t * current = head;
+	while (current -> next != NULL){
+     		current = current -> next;	
+	}
+	current -> next = malloc(sizeof(node_t));
+	current -> next -> process = process;
+	current -> next -> next = NULL;
+}    
 
+// Remove head from the list which is the first item added
 node_t *pop(node_t **head){
-    *get_node = NULL;
+	*get_node = NULL;
 	node_t *next_node = NULL;
 	if (*head == NULL){
 		return -1;
