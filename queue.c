7d55687a10eb14cd69516a20daebdc24c900c67b
Yin Zhou <31861121+yinz87@users.noreply.github.com>
2019-03-04 21:42:06 -0500

    Update queue.c
    update

--- queue.c
+++ queue.c
@@ -12,14 +12,32 @@
 // Define your FIFO queue functions here, these will most likely be the
 // push and pop functions that you declared in your header file
 
-void *push(node_t *tail, process proc){
+void *push(node_t **head, process proc){
     node_t *new_node = (node_t*) malloc(sizeof(node_t));
-    node_t *current_node = tail;
+    node_t *current_node = head;
     while(current_node != NULL){
         node
     }
+    
+    //or
+    
+    node_t * new_node = node_t -> malloc(sizeof(node_t));
+	new_node -> process = process;
+	new_node -> next = *head;
+	*head = new_node;
+    
 }
 
-node_t *pop(node_t *tail){
+node_t *pop(node_t **head){
+    *get_node = NULL;
+	node_t *next_node = NULL;
+	if (*head == NULL){
+		return -1;
+	}
+	next_node = (*head)->next;
+	*get_node = head -> process;
+	free(*head);
+	*head = next_node;
 
+	return get_node;
 }
